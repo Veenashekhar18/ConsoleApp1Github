2ac0eeba8f6439c7513be04c22305b3362ad139a
Veenashekhar18 <veenaraichur58@gmail.com>
2023-07-26 11:28:19 +0530

    2nd commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        public void add()
+        {
+            int a = 23;
+            int b = 34;
+            int c = a + b;
+            Console.WriteLine("added numbers are"+c); 
+
+        }
+    }
+}
